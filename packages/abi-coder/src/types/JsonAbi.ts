9f50ee9cfe847a1c84a9cc68dc6d1c5e4ff95775
nedsalk <nedim.salkic@fuel.sh>
2024-07-17 11:12:22 +0200

    define the new spec type

--- packages/abi-coder/src/types/JsonAbi.ts
+++ packages/abi-coder/src/types/JsonAbi.ts
@@ -11,6 +11,91 @@ export interface JsonAbi {
   readonly encoding?: string;
 }
 
+export interface JsonAbi2 {
+  specVersion: string;
+  encodingVersion: string;
+  programType: 'script' | 'contract' | 'predicate' | 'library';
+  concreteTypes: readonly ConcreteType[];
+  typeMetadata: readonly TypeMetadata[];
+  functions: readonly AbiFunction[];
+  loggedTypes: readonly LoggedType[];
+  messageTypes: readonly MessageType[];
+}
+
+export interface ConcreteType {
+  type: string;
+  concreteTypeId: string;
+  metadataTypeId?: number;
+  typeArguments?: readonly string[];
+}
+
+export interface TypeMetadata {
+  type: string;
+  metadataTypeId: number;
+  components: readonly Component[];
+  typeParameters?: readonly number[];
+}
+
+export interface Component extends TypeArgument {
+  name: string;
+}
+
+export interface TypeArgument {
+  typeId: string; // the type metadata declaration ID or type concrete declaration hash based ID of the type of the component.
+  typeArguments?: TypeArgument[];
+}
+
+export interface AbiFunction {
+  name: string;
+  inputs: readonly AbiFunctionInput[];
+  output: string;
+  attributes?: readonly AbiFunctionAttribute[];
+}
+
+export interface AbiFunctionInput {
+  name: string;
+  concreteTypeId: string;
+}
+
+export type AbiFunctionAttribute = Storage | Payable | Test | Inline | DocComment | Doc;
+
+interface Payable {
+  name: 'payable';
+}
+interface Storage {
+  name: 'storage';
+  arguments: readonly ('read' | 'write')[];
+}
+interface Test {
+  name: 'test';
+}
+interface Inline {
+  name: 'inline';
+  arguments: 'never' | 'always';
+}
+interface DocComment {
+  name: 'doc-comment';
+  arguments: string;
+}
+interface Doc {
+  name: 'doc';
+}
+
+export interface LoggedType {
+  logId: string;
+  loggedType: string; // the _type concrete declaration_ hash based ID of the value being logged.
+}
+
+export interface MessageType {
+  message_id: string;
+  messageDataType: string;
+}
+export interface Configurable {
+  name: string;
+  configurableType: string;
+  offset: number;
+}
+
 export interface JsonAbiType {
   readonly typeId: number;
   readonly type: string;
